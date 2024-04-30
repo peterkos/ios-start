744e176db1c74555fbcd61a03f2fd923e2535b63
Peter Kos <pkos91@icloud.com>
2024-04-30 14:31:32 -0400

    init

---
+++ NewProject/NewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewProjectApp.swift
+//  NewProject
+//
+//  Created by Peter Kos on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct NewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
